4cac8abbfc2bb303b59e84ace1b1952b34499148
jc <jamal@objex.ca>
2016-10-10 19:40:00 -0400

    module 2

--- app.js
+++ app.js
@@ -1,32 +1,84 @@
-(function () {
+(function(){
     'use strict';
+    angular.module('MallmartCheckOff', [])
 
-    angular.module('LunchChecker', [])
-        .controller('LunchCheckController', LunchCheckController);
-        // LunchCheckController.$inject=[$scope];
-        function LunchCheckController($scope){
-            $scope.message = "";
-            $scope.list="";
-            $scope.calcMenu = function (){
+        .service('ToBuyService', BaseListService)
+        .service('AlreadyBoughtService', BaseListService)
+        .controller('AddController', AddController)
+        .controller('ShowController', ShowController)
+        .controller('AlreadyBoughtController', AlreadyBoughtController);
 
-                var list1 = $scope.list;
-                if (list1 == "") {
-                    $scope.message = "Please enter data first";
-                } else {
-                    var length1 = calcLength(list1);
-                    if (length1 <=3) {
-                        $scope.message = "Enjoy!";
-                    } else {
-                        $scope.message = "Too much!";
-                    }
-                }
+    AddController.$inject = ['ToBuyService'];
+    function AddController(ToBuyService) {
+        var controller = this;
+        controller.itemName = ""
+        controller.itemQuantity = ""
+        controller.addItem = function() {
+            if (controller.itemName!="") {
+                ToBuyService.addItem(controller.itemName, controller.itemQuantity);
+                console.log(controller.itemName);
+            } else {
+                console.log("itemNmae is empty");
             }
+        };
+    };
 
-        }
+    ShowController.$inject = ['ToBuyService', 'AlreadyBoughtService'];
+    function ShowController(ToBuyService, AlreadyBoughtService) {
+        var controller = this;
 
-        function calcLength(list1){
-            var array = list1.split(",");
-            console.log(array.length);
-            return array.length;
-        }
+        controller.items = ToBuyService.getItems();
+
+        controller.buyItem = function(index) {
+            var item = ToBuyService.getItem(index);
+            AlreadyBoughtService.addWholeItem(item);
+
+            ToBuyService.removeItem(index);
+        };
+
+        controller.isEmpty = function() {
+            return ToBuyService.getItems().length === 0;
+        };
+    };
+
+    AlreadyBoughtController.$inject = ['$scope', 'AlreadyBoughtService'];
+    function AlreadyBoughtController($scope, AlreadyBoughtService) {
+        var controller = this;
+
+        controller.items = AlreadyBoughtService.getItems();
+    };
+
+    function BaseListService() {
+        var service = this;
+        var itemList = [];
+
+        service.addWholeItem = function(item) {
+            itemList.push(item);
+        };
+
+        service.addItem = function(name, quantity) {
+            var item = {
+                name : name,
+                quantity : quantity
+            };
+            itemList.push(item);
+        };
+
+        service.removeItem = function(index) {
+            itemList.splice(index, 1);
+        };
+
+        service.getItems = function() {
+            return itemList;
+        };
+
+        service.getItem = function(index) {
+            return itemList[index];
+        };
+
+        service.setItems = function(items) {
+            service.itemList = items;
+        };
+    };
+
 })();
